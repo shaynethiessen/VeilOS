08a3b86609bf8dc1db22933c198e0c5aaebda160
Shayne Thiessen <shayne@shaynethiessen.com>
2025-09-23 10:16:59 -0500

    KRDC

--- install/devops.sh
+++ install/devops.sh
@@ -5,8 +5,9 @@ info "Setting up DevOps..."
 
 info "Installing yakuake: Drop-down terminal emulator for KDE."
 info "Installing testdisk: Data recovery and disk partition repair tool."
+info "Installing krdc: Remote Desktop Connection client."
 
 apt-get install -y --no-install-recommends \
-  yakuake testdisk
+  yakuake testdisk krdc
 
 info "DevOps Complete!"
